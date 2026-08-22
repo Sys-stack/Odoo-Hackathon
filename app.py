a8dbe23b6fd58d84735e3cea60ebd22ff3e301d0
Sys <shivasudharsank2008@gmail.com>
2026-08-22 09:44:43 +0530

    Updated Requirements and added routes

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.get("/")
-def index():
-    return "GlobalThrotter is running"
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
